1969cd030b20bef8c5249b171bebcf58a4efbfb7
Renan Caciolatto <renancaciolatto@hotmail.com>
2023-11-09 00:09:40 -0300

    Banco de Dados H2, perfil teste, JPA

--- src/main/java/com/renancaciolatto/curso/entities/Usuario.java
+++ src/main/java/com/renancaciolatto/curso/entities/Usuario.java
@@ -3,9 +3,17 @@ package com.renancaciolatto.curso.entities;
 import java.io.Serializable;
 import java.util.Objects;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
 public class Usuario implements Serializable{
 	private static final long serialVersionUID = 1L;
 	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	private String nome;
 	private String email;
